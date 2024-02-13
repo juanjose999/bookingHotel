1c3a2a70ba10fa8cc35b908c189d9149dd0b9300
juan jose s.o <juanjose000012@gmail.com>
2024-02-13 08:56:27 -0500

    full api is ok

--- src/main/java/com/hotel/booking/controller/HotelController.java
+++ src/main/java/com/hotel/booking/controller/HotelController.java
@@ -1,15 +1,13 @@
 package com.hotel.booking.controller;
 
-<<<<<<< HEAD
+
 import com.hotel.booking.model.dto.booking.BookingDto;
 import com.hotel.booking.model.dto.booking.BookingResponseDto;
 import com.hotel.booking.model.dto.hotel.HotelDto;
 import com.hotel.booking.model.dto.hotel.HotelResponseDto;
 import com.hotel.booking.service.booking.BookingService;
-=======
 import com.hotel.booking.model.dto.hotel.HotelDto;
 import com.hotel.booking.model.dto.hotel.HotelResponseDto;
->>>>>>> 45ab499fb01d56f88062dacc615d034d503b3f50
 import com.hotel.booking.service.hotel.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,101 +16,60 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-<<<<<<< HEAD
 @RestController
 @RequestMapping("/v1/hotels")
 public class HotelController {
+
     private final HotelService hotelService;
+
     @Autowired
-    public HotelController(HotelService hotelService){
-        this.hotelService=hotelService;
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
     }
 
     @GetMapping
-    public ResponseEntity<List<HotelResponseDto>> getAllHotels(){
-        try{
+    public ResponseEntity<List<HotelResponseDto>> getAllHotels() {
+        try {
             List<HotelResponseDto> hotelResponseDto = hotelService.getAllHotels();
             return ResponseEntity.ok(hotelResponseDto);
-        }catch (Exception e){
-            System.err.println("Error in getAllHotel: " + e.getMessage());
-=======
-
-@RestController
-@RequestMapping("/v1/hotel")
-public class HotelController {
-    @Autowired
-    private HotelService hotelService;
-
-    @GetMapping
-    public ResponseEntity<List<HotelResponseDto>> getAllHotel(){
-        try{
-            List<HotelResponseDto> hoteles = hotelService.getAllHotel();
-            return ResponseEntity.ok(hoteles);
-        }catch (Exception e){
-            System.err.println("error in the getAllHotels: " + e.getMessage());
->>>>>>> 45ab499fb01d56f88062dacc615d034d503b3f50
+        } catch (Exception e) {
+            System.err.println("Error in getAllHotels: " + e.getMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
-<<<<<<< HEAD
     @GetMapping("/{id}")
-    public ResponseEntity<HotelResponseDto> findHotelById(@PathVariable String idHotel){
-        try{
-            return new ResponseEntity<>(hotelService.findHotelById(idHotel),HttpStatus.OK);
-        }catch (NoSuchElementException e){
-            return new ResponseEntity("the hotel + " +  idHotel + " does't in the data base.", HttpStatus.NOT_FOUND);
+    public ResponseEntity<HotelResponseDto> findHotelById(@PathVariable String idHotel) {
+        try {
+            return new ResponseEntity<>(hotelService.findHotelById(idHotel), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity("The hotel " + idHotel + " doesn't exist in the database.", HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping
-    public ResponseEntity<HotelResponseDto> saveHotel(@RequestBody HotelDto hotelDto){
-        return new ResponseEntity<>(hotelService.saveHotel(hotelDto),HttpStatus.CREATED);
+    public ResponseEntity<HotelResponseDto> saveHotel(@RequestBody HotelDto hotelDto) {
+        return new ResponseEntity<>(hotelService.saveHotel(hotelDto), HttpStatus.CREATED);
     }
 
     @PutMapping("/{idHotel}")
-    public ResponseEntity<HotelResponseDto> updateHotel(@PathVariable String idHotel, @RequestBody HotelDto hotelDto){
-        try{
-            Boolean isUpdateHotel = hotelService.updateHotel(idHotel,hotelDto);
-=======
-    @GetMapping("/{HotelId}")
-    public ResponseEntity<HotelResponseDto> findHotelById(@PathVariable String hotelId){
-        try{
-            return new ResponseEntity<>(hotelService.findHotelById(hotelId), HttpStatus.OK);
-        }catch (NoSuchElementException e){
-            return new ResponseEntity("the user " + hotelId + " doesn't in the data base.", HttpStatus.NOT_FOUND);
-        }
-    }
+    public ResponseEntity<HotelResponseDto> updateHotel(@PathVariable String idHotel, @RequestBody HotelDto hotelDto) {
+        try {
+            Boolean isUpdateHotel = hotelService.updateHotel(idHotel, hotelDto);
 
-    @PostMapping("/{hotelId}")
-    public ResponseEntity<Boolean> updateHotel(@PathVariable String hotelId, @RequestBody HotelDto hotelDto){
-        try{
-            Boolean isUpdateHotel = hotelService.updateHotel(hotelId, hotelDto);
->>>>>>> 45ab499fb01d56f88062dacc615d034d503b3f50
-            if(isUpdateHotel){
-                return new ResponseEntity("the hotel update is ok.", HttpStatus.OK);
-            }else {
+            if (isUpdateHotel) {
+                return new ResponseEntity("The hotel update is successful.", HttpStatus.OK);
+            } else {
                 return new ResponseEntity(HttpStatus.NOT_FOUND);
             }
-        }catch (NoSuchElementException e){
-<<<<<<< HEAD
-            return new ResponseEntity("the hotel " + idHotel + " doesn't in the data base." , HttpStatus.NOT_FOUND);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity("The hotel " + idHotel + " doesn't exist in the database.", HttpStatus.NOT_FOUND);
         }
     }
 
     @DeleteMapping("/{idHotel}")
-    public ResponseEntity<Boolean> deleteBooking(@PathVariable String idHotel){
-        return new ResponseEntity<>(hotelService.deleteHotel(idHotel),HttpStatus.OK);
+    public ResponseEntity<Boolean> deleteHotel(@PathVariable String idHotel) {
+        return new ResponseEntity<>(hotelService.deleteHotel(idHotel), HttpStatus.OK);
     }
 
-=======
-            return new ResponseEntity("the user " + hotelId + " doesn't in the data base." , HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/{hotelId}")
-    public ResponseEntity<Boolean> deleteUser(@PathVariable String hotelId){
-        return new ResponseEntity<>(hotelService.deleteHotel(hotelId), HttpStatus.OK);
-    }
->>>>>>> 45ab499fb01d56f88062dacc615d034d503b3f50
 }
